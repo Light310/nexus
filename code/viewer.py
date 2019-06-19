40bb771a1a28a6bc2b0b045d3981e4e4cabd6156
Fenix <ks@mail.com>
2019-06-19 21:16:43 +0200

    ui + wsgi + nginx

--- code/viewer.py
+++ code/viewer.py
@@ -3,6 +3,7 @@ import zmq
 import base64
 import numpy as np
 from datetime import datetime
+from shutil import copyfile
 
 context = zmq.Context()
 footage_socket = context.socket(zmq.SUB)
@@ -15,9 +16,11 @@ while True:
         img = base64.b64decode(frame)
         npimg = np.fromstring(img, dtype=np.uint8)
         source = cv2.imdecode(npimg, 1)
-        file = '/nexus/static/images/img.jpg'
+        file = '/nexus/static/images/img_tmp.jpg'
+        out_file = '/nexus/static/images/img.jpg'
         print(datetime.now())
         cv2.imwrite(file, source)
+        copyfile(file, out_file)
         #cv2.imshow("Stream", source)
         #cv2.waitKey(1)
 
